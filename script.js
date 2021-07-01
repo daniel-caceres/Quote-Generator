1cb59cd76426a28987998df4a9e3e2f20ac59801
Daniel Caceres <pedrodanielcaceres@yahoo.com.ar>
2021-07-01 17:50:43 -0300

    Create the loader functions and use them.

--- script.js
+++ script.js
@@ -3,50 +3,68 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const btnNewQuote = document.getElementById('new-quote');
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 function newQuote() {
     // Pick a random quote from apiQuotes array.
     const index = Math.floor(Math.random() * apiQuotes.length);
     const quote = apiQuotes[index];
 
-    // Check if Author is blank and replace it with 'Unknown'
+    // Check if Author is blank and replace it with 'Unknown'.
     if (!quote.author) {
         authorText.textContent = 'Unknown';    
     } else {
         authorText.textContent = quote.author;    
     }
-    // Check Quote Length to determine styling
+    // Check Quote Length to determine styling.
     if (quote.text.length > 100) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader.
     quoteText.textContent = quote.text;    
+    // Hide the loader.
+    complete();
 }
 
 // Get Quotes From API.
 async function getQuotes() {
+    // Show the loader.
+    loading()
     const url = "https://type.fit/api/quotes";
     try {
         const response = await fetch(url);
         apiQuotes = await response.json();
-        console.log(newQuote());        
+        newQuote();        
     } catch (error) {
-        console.log("Hubo un error " + error);
+        alert("Hubo un error ");
     }
 }
 
-// Tweet Quote
+// Tweet Quote.
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
 
     // Open a new window at twitter.
-    window.open(twitterUrl, '_blank');
+    window.open(twitterUrl,  '_blank');
 }
 
-// Event Listeners
+// Event Listeners.
 btnNewQuote.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
