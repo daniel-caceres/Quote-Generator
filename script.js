f04fbbdc7c72e06b6eaf6fc48ff3bc98241d0326
Daniel Caceres <pedrodanielcaceres@yahoo.com.ar>
2021-07-01 17:18:52 -0300

    Print quote and author

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const btnNewQuote = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 function newQuote() {
     // Pick a random quote from apiQuotes array.
     const index = Math.floor(Math.random() * apiQuotes.length);
     const quote = apiQuotes[index];
-    
-    return quote;
+
+    // Check if Author is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';    
+    } else {
+        authorText.textContent = quote.author;    
+    }
+    // Check Quote Length to determine styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;    
 }
 
 // Get Quotes From API.
@@ -16,7 +34,7 @@ async function getQuotes() {
         apiQuotes = await response.json();
         console.log(newQuote());        
     } catch (error) {
-        console.log("Hubo un error");
+        console.log("Hubo un error " + error);
     }
 }
 
